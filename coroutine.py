251bea73ad5394495ecbcbbda9bf8cac6b0d165e
wangyuehua <wangyuehua@tencent.com>
2022-03-23 07:59:00 +0800

    add coroutine

--- coroutine.py
+++ coroutine.py
@@ -5,13 +5,87 @@
 # 协程 coroutine object
 import asyncio
 
-async def crawl_page(url):
-    print('crawling {}'.format(url))
-    sleep_time = int(url.split('_')[-1])
-    await asyncio.sleep(sleep_time)
-    print('OK {}'.format(url))
+import time
+import asyncio
+import aiohttp
+import requests
+from bs4 import BeautifulSoup
 
-async def main(urls):
-    for url in urls:
-        await crawl_page(url)
+# modify function
+def timer(func):
+    def func_wrapper(*args, **kwargs):
+        from time import time
+        time_start = time()
+        result = func(*args, **kwargs)
+        time_end = time()
+        time_spend = time_end - time_start
+        print('%s cost time: %.3f s' % (func.__name__, time_spend))
+        return result
+    return func_wrapper
+
+
+# use 同步编程
+@timer
+def main2():
+    url = "https://movie.douban.com/cinema/later/beijing/"
+    init_page = requests.get(url).content
+    print('done')
+    init_soup = BeautifulSoup(init_page, 'lxml')
+
+    all_movies = init_soup.find('div', id="showing-soon")
+    for each_movie in all_movies.find_all('div', class_="item"):
+        all_a_tag = each_movie.find_all('a')
+        all_li_tag = each_movie.find_all('li')
+
+        movie_name = all_a_tag[1].text
+        url_to_fetch = all_a_tag[1]['href']
+        movie_date = all_li_tag[0].text
+
+        response_item = requests.get(url_to_fetch).content
+        soup_item = BeautifulSoup(response_item, 'lxml')
+        img_tag = soup_item.find('img')
+
+        print('{} {} {}'.format(movie_name, movie_date, img_tag['src']))
+main2()
+
+
+# use 异步编程
+async def fetch_content(url):
+    async with aiohttp.ClientSession(
+        headers=header, connector=aiohttp.TCPConnector(ssl=False)
+    ) as session:
+        async with session.get(url) as response:
+            return await response.text()
+
+@timer
+async def main():
+    url = "https://movie.douban.com/cinema/later/beijing/"
+    init_page = await fetch_content(url)
+    init_soup = BeautifulSoup(init_page, 'lxml')
+
+    movie_names, urls_to_fetch, movie_dates = [], [], []
+
+    all_movies = init_soup.find('div', id="showing-soon")
+    for each_movie in all_movies.find_all('div', class_="item"):
+        all_a_tag = each_movie.find_all('a')
+        all_li_tag = each_movie.find_all('li')
+
+        movie_names.append(all_a_tag[1].text)
+        urls_to_fetch.append(all_a_tag[1]['href'])
+        movie_dates.append(all_li_tag[0].text)
+
+    tasks = [fetch_content(url) for url in urls_to_fetch]
+    pages = await asyncio.gather(*tasks)
+
+    for movie_name, movie_date, page in zip(movie_names, movie_dates, pages):
+        soup_item = BeautifulSoup(page, 'lxml')
+        img_tag = soup_item.find('img')
+
+        print('{} {} {}'.format(movie_name, movie_date, img_tag['src']))
+
+# asyncio.run(main())
+
+
+
+
 
